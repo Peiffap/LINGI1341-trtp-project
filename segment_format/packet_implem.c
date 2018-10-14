466cdac876f3183e54fba2c9dfa023404f91debd
Minh-Phuong Tran <minh-phuong.tran@student.uclouvain.be>
2018-10-14 16:53:45 +0200

    segment : ne fonctionne pas encore

--- segment_format/packet_implem.c
+++ segment_format/packet_implem.c
@@ -1,10 +1,23 @@
 #include "packet_interface.h"
-
+#include <zlib.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <string.h>
 /* Extra #includes */
 /* Your code will be inserted here */
 
 struct __attribute__((__packed__)) pkt {
-	/* Your code will be inserted here */
+	uint8_t window : 5;	
+	uint8_t tr : 1;
+	ptypes_t type : 2;
+	uint8_t seqnum;
+	uint16_t length;
+	uint32_t timestamp;
+	uint32_t crc1;
+	char* payload;
+	uint32_t crc2;
+
 };
 
 /* Extra code */
@@ -12,113 +25,265 @@ struct __attribute__((__packed__)) pkt {
 
 pkt_t* pkt_new()
 {
-	/* Your code will be inserted here */
+	return calloc(1, sizeof(pkt_t));
 }
 
 void pkt_del(pkt_t *pkt)
 {
-    /* Your code will be inserted here */
+ 	if(pkt!=NULL){
+		if(pkt->payload !=NULL){
+			free(pkt->payload);
+		}
+		free(pkt);	
+	}
 }
 
 pkt_status_code pkt_decode(const char *data, const size_t len, pkt_t *pkt)
 {
-	/* Your code will be inserted here */
+	
+	// window, tr et type 		
+	memcpy(pkt, data, sizeof(uint8_t));
+	
+	if((pkt_get_type(pkt) != PTYPE_DATA)&&(pkt_get_type(pkt) != PTYPE_ACK)&&(pkt_get_type(pkt) != PTYPE_NACK)){
+		return E_TYPE;
+	}
+	// seqnum
+	uint8_t seqnum;
+	memcpy(&seqnum, data+sizeof(uint8_t),sizeof(uint8_t)); 
+	pkt_set_seqnum(pkt, seqnum);
+
+	// length
+	uint16_t length;
+	memcpy(&length, data+sizeof(uint16_t),sizeof(uint16_t)); 
+	length = ntohs(length);
+	pkt_set_length(pkt, length);
+
+	if((length != len-4*sizeof(uint32_t))&&(length != len-3*sizeof(uint32_t))){
+		return E_LENGTH;
+	}
+	
+	if((pkt->tr == 1)&&(length!=0)){
+		return E_TR;
+	}
+	// timestamp
+	uint32_t timestamp;
+	memcpy(&timestamp, data+sizeof(uint32_t),sizeof(uint32_t)); 
+	pkt_set_timestamp(pkt, timestamp);
+	
+	// crc1
+	uint32_t crc1;
+	memcpy(&crc1, data+2*sizeof(uint32_t), sizeof(uint32_t));
+	crc1 = ntohl(crc1);
+	pkt_set_crc1(pkt, crc1);
+
+	// verif crc1
+	uint32_t testCrc1 = 0;
+	char *dataNonTr = (char *)malloc(2*sizeof(uint32_t));
+	memcpy(dataNonTr, data, (2*sizeof(uint32_t)));
+	dataNonTr[0] = dataNonTr[0] & 0b11011111;
+	testCrc1 = crc32(testCrc1, (Bytef *)dataNonTr, (2*sizeof(uint32_t)));
+
+	if(testCrc1 != crc1){
+		return E_CRC;
+	}
+	// payload
+	pkt_set_payload(pkt, data+3*sizeof(uint32_t), length);
+
+
+	if(length == len-4*sizeof(uint32_t)){
+		// crc2
+		uint32_t crc2;
+		memcpy(&crc2, &(data[length-4]), sizeof(uint32_t));
+		crc2 = ntohl(crc2);
+		pkt_set_crc2(pkt, crc2);
+		
+		// verif crc2
+		uint32_t testCrc2 = 0;
+		testCrc2 = crc32(testCrc2, (Bytef *)(data +(3*sizeof(uint32_t))), length);
+
+		if(testCrc2 != crc2){
+			return E_CRC;
+		}
+	}
+
+	return PKT_OK;
+	
 }
 
 pkt_status_code pkt_encode(const pkt_t* pkt, char *buf, size_t *len)
 {
-	/* Your code will be inserted here */
+
+	if((pkt_get_crc2(pkt) == 0)&&(*len<pkt_get_length(pkt)+3*sizeof(uint32_t))){
+		return E_NOMEM;
+	}
+	else if((pkt_get_crc2(pkt)!=0)&&(*len<pkt_get_length(pkt)+4*sizeof(uint32_t))){
+		return E_NOMEM;
+	}
+	// window, tr et type 
+	memcpy(buf, pkt, sizeof(uint8_t));
+	
+	// seqnum
+	uint8_t seqnum = htons(	pkt_get_seqnum(pkt));
+	memcpy(buf+sizeof(uint8_t), &seqnum,sizeof(uint8_t)); 
+	
+	// length
+	uint16_t length = pkt_get_length(pkt);
+	uint16_t nlength = htons(length);
+	memcpy(buf+sizeof(uint16_t), &nlength,sizeof(uint16_t)); 
+	
+	// timestamp
+	uint32_t timestamp = htonl(pkt_get_timestamp(pkt));
+	memcpy(buf+sizeof(uint32_t), &timestamp,sizeof(uint32_t)); 
+
+	// crc1
+	uint32_t crc1 = htonl(pkt_get_crc1(pkt));
+	memcpy(buf+2*sizeof(uint32_t), &crc1,sizeof(uint32_t)); 
+	
+	// payload
+	memcpy(buf+3*sizeof(uint32_t), pkt_get_payload(pkt), length);
+
+
+	// crc2
+	uint32_t crc2 = htonl(pkt_get_crc2(pkt));
+	if(crc2!=0){
+
+		memcpy(buf+3*sizeof(uint32_t)+length, &crc2, sizeof(uint32_t));
+	}
+	return PKT_OK;
+	
+	
+
 }
 
-ptypes_t pkt_get_type  (const pkt_t*)
+ptypes_t pkt_get_type  (const pkt_t* pkt)
 {
-	/* Your code will be inserted here */
+	return pkt->type;
 }
 
-uint8_t  pkt_get_tr(const pkt_t*)
+uint8_t  pkt_get_tr(const pkt_t* pkt)
 {
-	/* Your code will be inserted here */
+	return pkt->tr;
 }
 
-uint8_t  pkt_get_window(const pkt_t*)
+uint8_t  pkt_get_window(const pkt_t* pkt)
 {
-	/* Your code will be inserted here */
+	return pkt->window;
 }
 
-uint8_t  pkt_get_seqnum(const pkt_t*)
+uint8_t  pkt_get_seqnum(const pkt_t* pkt)
 {
-	/* Your code will be inserted here */
+	return pkt->seqnum;
 }
 
-uint16_t pkt_get_length(const pkt_t*)
+uint16_t pkt_get_length(const pkt_t* pkt)
 {
-	/* Your code will be inserted here */
+	return pkt->length;
 }
 
-uint32_t pkt_get_timestamp   (const pkt_t*)
+uint32_t pkt_get_timestamp   (const pkt_t* pkt)
 {
-	/* Your code will be inserted here */
+	return pkt->timestamp;
 }
 
-uint32_t pkt_get_crc1   (const pkt_t*)
+uint32_t pkt_get_crc1   (const pkt_t* pkt)
 {
-	/* Your code will be inserted here */
+	return pkt->crc1;
 }
 
-uint32_t pkt_get_crc2   (const pkt_t*)
+uint32_t pkt_get_crc2   (const pkt_t* pkt)
 {
-	/* Your code will be inserted here */
+	return pkt->crc2;
 }
 
-const char* pkt_get_payload(const pkt_t*)
+const char* pkt_get_payload(const pkt_t* pkt)
 {
-	/* Your code will be inserted here */
+	return pkt->payload;
 }
 
 
 pkt_status_code pkt_set_type(pkt_t *pkt, const ptypes_t type)
 {
-	/* Your code will be inserted here */
+	if ((type!= PTYPE_DATA)&&(type!= PTYPE_ACK)&&(type!= PTYPE_NACK))	{
+		return E_TYPE;
+	}
+	pkt->type = type;
+	return PKT_OK;
 }
 
 pkt_status_code pkt_set_tr(pkt_t *pkt, const uint8_t tr)
 {
-	/* Your code will be inserted here */
+	if ((pkt->type!= PTYPE_DATA)&&(tr==1))	{
+		return E_TR;
+	}
+	pkt->tr = tr;
+	return PKT_OK;
 }
 
 pkt_status_code pkt_set_window(pkt_t *pkt, const uint8_t window)
 {
-	/* Your code will be inserted here */
+	if (window >31)	{
+		return E_WINDOW;
+	}
+	pkt->window = window;
+	return PKT_OK;
 }
 
 pkt_status_code pkt_set_seqnum(pkt_t *pkt, const uint8_t seqnum)
 {
-	/* Your code will be inserted here */
+	/*if 	{
+		return E_SEQNUM;
+	} Quand a-t-on un probleme ? */
+	pkt->seqnum = seqnum;
+	return PKT_OK;
 }
 
 pkt_status_code pkt_set_length(pkt_t *pkt, const uint16_t length)
 {
-	/* Your code will be inserted here */
+	if (length>512)	{
+		return E_LENGTH;
+	}
+	pkt->length = length;
+	return PKT_OK;
 }
 
 pkt_status_code pkt_set_timestamp(pkt_t *pkt, const uint32_t timestamp)
 {
-	/* Your code will be inserted here */
+	
+	pkt->timestamp = timestamp;
+	return PKT_OK;
 }
 
 pkt_status_code pkt_set_crc1(pkt_t *pkt, const uint32_t crc1)
 {
-	/* Your code will be inserted here */
+	pkt->crc1 = crc1;
+	return PKT_OK;
 }
 
 pkt_status_code pkt_set_crc2(pkt_t *pkt, const uint32_t crc2)
 {
-	/* Your code will be inserted here */
+	pkt->crc2 = crc2;
+	return PKT_OK;
 }
 
 pkt_status_code pkt_set_payload(pkt_t *pkt,
 							    const char *data,
 								const uint16_t length)
 {
-	/* Your code will be inserted here */
+	if(pkt->payload !=NULL){
+	    free(pkt->payload);
+	    pkt->payload = NULL;
+	    pkt->length = 0;
+	}
+       
+
+	pkt->payload = (char *)malloc((length)*sizeof(char));
+
+	if(pkt->payload == NULL){
+		return E_NOMEM;
+	}
+	memcpy(pkt->payload, data, length);
+	pkt->length = length;
+
+
+	return PKT_OK;
 }
