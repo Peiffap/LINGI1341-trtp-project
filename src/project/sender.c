8cedee4167e5bb451e6c5c6f6220fab5435ce0d4
Minh-Phuong Tran <minh-phuong.tran@student.uclouvain.be>
2018-10-21 00:09:19 +0200

    sender v1.3
    + disconnection

--- src/project/sender.c
+++ src/project/sender.c
@@ -27,23 +27,23 @@ struct dataqueue {
 struct dataqueue *startofqueue = NULL;
 struct dataqueue *firsttosend = NULL;
 struct dataqueue *lasttosend = NULL;
+uint8_t lastackseqnum = 0;
 
-int remove_pkt(uint8_t seqnum){
+int remove_pkt(uint8_t seqnum, int operation){
 	struct dataqueue *current = startofqueue;
-	if(current->seqnum == seqnum){
+	if(((operation == 0)&&(current->seqnum == seqnum))||((operation == -1)&&(current->seqnum <= seqnum))){
 		startofqueue = current->next;
+		free(current);
 		return 0;
 	}
 	else{
 		current = current->next;
 		struct dataqueue *before = startofqueue;
-
-		int search = 1;
-		while((search)&&(current!=NULL)){
-			if(current->seqnum == seqnum){
+		while(current!=NULL){
+			if(((operation == 0)&&(current->seqnum == seqnum))||((operation == -1)&&(current->seqnum <= seqnum))){
 
 				before->next = current->next;
-				search = 0;
+				free(current);
 				return 0;
 
 			}
@@ -59,7 +59,7 @@ int remove_pkt(uint8_t seqnum){
 	return -1;
 }
 
-void send_pkt(const int sfd){
+int send_pkt(const int sfd){
 
 	if((firsttosend != NULL)&&
 		(((firsttosend->seqnum >= firstseqnumwindow)
@@ -71,23 +71,25 @@ void send_pkt(const int sfd){
 		int err = write(sfd, firsttosend->bufpkt, firsttosend->len);
 		if(err <0){
 			perror("error writing pkt");
+			return -1;
 		}
 
 		err = clock_gettime(CLOCK_MONOTONIC,&(firsttosend->time));
 		
 		if(err!=0){
 			perror("error clock in sending data");
+			return -1;
 		}
 		firsttosend = firsttosend->next;
 	}
 
 	// Else need an ack
 
-
+	return 0;
 }
 
 
-void add_pkt_to_queue( char* buf, int len){
+int add_pkt_to_queue( char* buf, int len){
 	int timestamp = 0; // How do we use the timestamp ??
 	pkt_t * newpkt = pkt_create(window, lastseqnum, len, timestamp, buf);
 	char bufpkt[len+4*sizeof(uint32_t)];
@@ -95,9 +97,14 @@ void add_pkt_to_queue( char* buf, int len){
 	pkt_status_code ret = pkt_encode(newpkt, bufpkt, &totlen);
 	if(ret != PKT_OK){
 		perror("error encoding pkt in add_pkt_to_queue");
+		return -1;
 	}
-
+	pkt_del(newpkt);
 	struct dataqueue *newdata = (struct dataqueue *)malloc(sizeof(struct dataqueue));
+	if(newdata==NULL){
+		perror("error malloc in add pkt to queue");
+		return -1;
+	}
 	lasttosend->next = newdata;
 	lasttosend = newdata;		
 	lasttosend->bufpkt = bufpkt;
@@ -111,25 +118,131 @@ void add_pkt_to_queue( char* buf, int len){
 	}
 
 	lastseqnum = (lastseqnum + 1)%MAX_SEQNUM;
+	return 0;
+
+}
+
+
+int disconnect(int sfd){
+
+	int timestamp = 0; // How do we use the timestamp ??
+	pkt_t * newpkt = pkt_create(window, lastackseqnum, 0, timestamp, NULL);
+	char bufpkt[3*sizeof(uint32_t)];
+	size_t totlen = 3*sizeof(uint32_t);
+	pkt_status_code ret = pkt_encode(newpkt, bufpkt, &totlen);
+	if(ret != PKT_OK){
+		perror("error encoding pkt in add_pkt_to_queue");
+		return -1;
+	}
+	pkt_del(newpkt);
+
+	int errw = write(sfd, bufpkt, totlen);
+	if(errw <0){
+		perror("error writing pkt in disconnect");
+
+		return -1;
+	}
+
+	struct timespec time1;
+	errw = clock_gettime(CLOCK_MONOTONIC,&time1);
 	
+	if(errw!=0){
+		perror("error clock in disconnect");
+
+		return -1;
+	}
+
+
+
+	struct timeval tv;
+	fd_set readfds;
+	tv.tv_sec= 0;
+	tv.tv_usec = 100;
+
+	char buf2[sizeof(pkt_t)];
+
+	int end = 0;
+	int err;
+	while (!end) {
+		memset((void *) buf2, 0, sizeof(pkt_t));
+		FD_ZERO(&readfds);
+		FD_SET(sfd, &readfds);
+
+		select(sfd + 1, &readfds, NULL, NULL, &tv);
+
+		if (FD_ISSET(sfd, &readfds)) {
+			err = read(sfd, buf2,  sizeof(pkt_t));
+
+			if (err <= 0){
+				perror("error reading from socket : ");
+				return -1;
+			}
+			pkt_t *ack = pkt_new();
+			err = pkt_decode(buf2, err, ack);
+			if(err != PKT_OK){
+				perror("error decoding ack");
+				pkt_del(ack);
+				return -1;
+			}
+			int seqnum = pkt_get_seqnum(ack);
+
+
+			if(pkt_get_type(ack) == PTYPE_ACK){
+				if(seqnum == lastackseqnum){
+					pkt_del(ack);
+					end = 1;
+					return 0;
+				}
+			}
+
+		}
+
+		struct timespec time;
+		int errc = clock_gettime(CLOCK_MONOTONIC,&time);
+		if(errc!=0){
+			perror("error get time in send data");
+			return -1;
+		}
+		if(((time.tv_sec - time1.tv_sec)*1000000 + (time.tv_nsec - time1.tv_nsec)/1000)>TIMEOUT){
+
+		//RESEND DATA
+
+			int errw1 = write(sfd, bufpkt, totlen);
+			if(errw1 <0){
+				perror("error writing pkt in resending pkt");
+				return -1;
+			}
+
+			errw1 = clock_gettime(CLOCK_MONOTONIC,&time1);
+			
+			if(errw1!=0){
+				perror("error clock in resending data");
+				return -1;
+			}
+
+		}
+		
+	}
+
+	return -1;
 
 }
 int send_data(const int sfd, const int fd){
 
 	struct timeval tv;
 	fd_set readfds;
-	tv.tv_sec= 6;
-	tv.tv_usec = 0;
+	tv.tv_sec= 0;
+	tv.tv_usec = 100;
 
 	char buf1[MAX_PAYLOAD_LENGTH];
-	char buf2[MAX_PAYLOAD_LENGTH];
+	char buf2[sizeof(pkt_t)+MAX_PAYLOAD_LENGTH];
 
 	int pkt_waiting = 0;
 
 	int err;
 	while (feof(stdin)) {
 		memset((void *) buf1, 0, MAX_PAYLOAD_LENGTH);
-		memset((void *) buf2, 0, MAX_PAYLOAD_LENGTH);
+		memset((void *) buf2, 0, sizeof(pkt_t)+MAX_PAYLOAD_LENGTH);
 		FD_ZERO(&readfds);
 		FD_SET(fd, &readfds);
 		FD_SET(sfd, &readfds);
@@ -147,34 +260,49 @@ int send_data(const int sfd, const int fd){
 				perror("error read fd in send data ");
 				return -1;
 			}
-			add_pkt_to_queue(buf1, err);
+			int erradd = add_pkt_to_queue(buf1, err);
+			if(erradd != 0){
+				perror("error adding pkt to queue");
+				return -1;
+			}
 			pkt_waiting ++;
 		}
 
 		if(pkt_waiting >0){
-			send_pkt(sfd);
+			int errsend = send_pkt(sfd);
+			if(errsend !=0){
+				perror("error sending pkt");
+				return -1;
+			}
 			
 		}
 
 		if (FD_ISSET(sfd, &readfds)) { // Data incoming from socket
-		    	err = read(sfd, buf2, sizeof(pkt_t));
+	    	err = read(sfd, buf2, sizeof(pkt_t)+MAX_PAYLOAD_LENGTH);
 
-		    	if (err <= 0){
+	    	if (err <= 0){
 				perror("error reading from socket : ");
 				return -1;
 			}
 			pkt_t *ack = pkt_new();
-			err = pkt_decode(buf2, sizeof(pkt_t), ack);
+			err = pkt_decode(buf2, err, ack);
 			if(err != PKT_OK){
 				perror("error decoding ack");
+				pkt_del(ack);
 				return -1;
 			}
 			int seqnum = pkt_get_seqnum(ack);
+			lastackseqnum = seqnum;
 
 			if(pkt_get_type(ack) == PTYPE_ACK){
 
 				firstseqnumwindow = (pkt_get_seqnum(ack)+1)%MAX_SEQNUM;	
-				int remerr = remove_pkt(seqnum);
+				int remerr = remove_pkt(seqnum, 0);
+				if(remerr == 0){
+					pkt_waiting--;
+
+				}
+				remerr = remove_pkt(seqnum, -1);
 				if(remerr == 0){
 					pkt_waiting--;
 
@@ -183,7 +311,7 @@ int send_data(const int sfd, const int fd){
 				const char *payload = pkt_get_payload(ack);
 
 				for(int i = 0; i<paylen; i++){
-					remerr = remove_pkt(payload[i]);
+					remerr = remove_pkt(payload[i], 0);
 					if(remerr == 0){
 						pkt_waiting--;
 
@@ -207,6 +335,8 @@ int send_data(const int sfd, const int fd){
 				int errw = write(sfd, current->bufpkt, current->len);
 				if(errw <0){
 					perror("error writing pkt in resending pkt");
+					pkt_del(ack);
+
 					return -1;
 				}
 
@@ -214,9 +344,13 @@ int send_data(const int sfd, const int fd){
 				
 				if(errw!=0){
 					perror("error clock in resending data");
+					pkt_del(ack);
+
 					return -1;
 				}
 			}
+
+			pkt_del(ack);
 			
 		}
 		
@@ -252,7 +386,11 @@ int send_data(const int sfd, const int fd){
 
 	}
 
-
+	int errdis = disconnect(sfd);
+	if(errdis !=0){
+		perror("error disconnecting");
+		return -1;
+	}
 
 	return 0;
 }
@@ -290,6 +428,7 @@ int main( int argc, char * argv[]){
  	
 	if (port <= 0){
 		perror("Invalid port");	
+		return -1;
 	}
 	struct sockaddr_in6 dest_addr;
 
@@ -311,6 +450,7 @@ int main( int argc, char * argv[]){
 		int fd = open(filename, O_RDONLY);
 		if( fd<0){
 			perror("Error opening the file");
+
 			return -1;
 		}
 		
@@ -318,8 +458,11 @@ int main( int argc, char * argv[]){
 
 		if(err1 ==-1){
 			perror("error sending data");
+			close(fd);
 			return -1;
 		}
+
+		close(fd);
 		
 	}
 	else {
